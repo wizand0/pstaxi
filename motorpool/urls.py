c46fef06176ac37b6bff7bb3fc592790a2ab2fe7
wizand0 <xabor@yandex.ru>
2022-08-02 18:25:01 +0300

    Новый дизайн и шаблоны

--- motorpool/urls.py
+++ motorpool/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "motorpool"
 
 urlpatterns = [
-    path('brand-list/', views.brand_list),
+    path('brand-list/', views.brand_list, name='brand_list'),
     path('brand-detail/<int:pk>/', views.brand_detail, name="brand_detail")
+
 ]
