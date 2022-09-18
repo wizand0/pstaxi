77808320da5a0e6306909cec56a956be13209bbf
wizand0 <xabor@yandex.ru>
2022-09-18 15:29:37 +0300

    Продолжеине обучения 20220918

--- motorpool/urls.py
+++ motorpool/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'motorpool'
 
 urlpatterns = [
-    path('brand-list/', views.brand_list, name='brand_list'),
-    path('brand-detail/<int:pk>/', views.BrandDetailView.as_view(), name='brand_detail')
+    path('brand-list/', views.BrandListView.as_view(), name='brand_list'),
+    path('brand-detail/<int:pk>/', views.BrandDetailView.as_view(), name='brand_detail'),
+    path('send-email/', views.send_email_view, name='send_email'),
 ]
 
--- motorpool/views.py
+++ motorpool/views.py
@@ -1,19 +1,26 @@
+from django.views.generic import DetailView, ListView
 from django.shortcuts import render
-from django.views.generic import DetailView
+from motorpool.forms import SendEmailForm
+from django.contrib import messages
+
 
 from motorpool.models import Brand
 
 
-def brand_list(request):
+class BrandListView(ListView):
+    model = Brand
     template_name = 'motorpool/brand_list.html'
-    brand_objects = Brand.objects.all()
-    brand_number = brand_objects.count()
-    context = {
-        'brand_objects': brand_objects,
-        'brand_number': brand_number,
-    }
+    paginate_by = 15
 
-    return render(request, template_name, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['brand_number'] = Brand.objects.count()
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.order_by('-pk')
+
 
 
 class BrandDetailView(DetailView):
@@ -36,5 +43,36 @@ class BrandDetailView(DetailView):
         default_template_names = super().get_template_names()
         return default_template_names
 
+def send_email_view(request):
+    if request.method == 'POST':
+        # Если метод запрос POST (нажата кнопка Отправить e-mail),
+        # то создаем экземпляр формы с данными из запроса
+        form = SendEmailForm(request.POST)
+        if form.is_valid():
+            # получаем поля формы, прошедшие валидацию
+            cd = form.cleaned_data
+            email = cd.get('email', '')
+            comment = cd.get('comment', '')
+            checkbox1 = cd.get('checkbox1', False)
+            checkbox2 = cd.get('checkbox2', False)
+            variant = int(cd.get('variant', 1))
+            variants = cd.get('variants', [])
+
+            action = request.POST.get('btn_action', '')
+            if action == 'action1':
+                print('Нажата кнопка 1')
+            elif action == 'action2':
+                print('Нажата кнопка 2')
+            else:
+                print('Нажата нераспознанная кнопка')
+        else:
+            messages.error(request, form.non_field_errors())
+    else:
+        # Если метод запрос GET (страница открыта в браузере),
+        # то создаем пустой экземпляр формы
+        form = SendEmailForm()
+
+    # Передаем форму в контекст с именем form
+    return render(request, 'motorpool/send_email.html', {'form': form})
 
 
