dd91b067f65a907d6c6af159b81e871113976df1
wizand0 <xabor@yandex.ru>
2022-09-18 12:11:37 +0300

    some fixies1

--- motorpool/views.py
+++ motorpool/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render
 from django.views.generic import DetailView
+
+from motorpool.models import Brand
 
 
 def brand_list(request):
@@ -15,4 +18,23 @@ def brand_list(request):
 
 class BrandDetailView(DetailView):
     model = Brand
+    template_name = 'motorpool/brand_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        default_object = super().get_object(queryset)
+        return default_object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cars'] = self.object.cars.all()
+        return context
+
+    def get_template_names(self):
+        default_template_names = super().get_template_names()
+        return default_template_names
+
+
 
